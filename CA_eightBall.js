75e89fc7d6f364d12cc0cb67a1c54337c07195f4
G. Cesana <55314921+cesanag@users.noreply.github.com>
2021-10-11 10:34:08 +0200

    Add files via upload

---
+++ CA_eightBall.js
@@ -0,0 +1,46 @@
+let userName = "Gips"
+if (userName !== "") {
+  console.log(`Hello, ${userName}!`)
+} else {
+  console.log("Hello!")
+}
+
+let userQuestion = "What's my future?"
+console.log(userQuestion)
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+switch (randomNumber) 
+		{
+    case 0: 
+    console.log('I don\'t know')
+    break; 
+    case 1: 
+    console.log('It is certain')
+    break; 
+    case 2: 
+    console.log('It is decidedly so')
+    break; 
+    case 3: 
+    console.log('Reply hazy try again')
+    break; 
+    case 4: 
+    console.log('Cannot predict now')
+    break; 
+    case 5: 
+    console.log('Do not count on it')
+    break; 
+    case 6: 
+    console.log('My sources say no')
+    break; 
+    case 7: 
+    console.log('Outlook not so good')
+    break; 
+    case 8: 
+    console.log('Signs point to yes')
+    break; 
+    }
+
+
+
+
